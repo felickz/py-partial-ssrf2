6003276a2c8703eabd88bb812f95bab1d507ce03
Chad Bentz <1760475+felickz@users.noreply.github.com>
2024-03-28 01:16:40 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-'''
-This is a multiline
-comment in Python
-'''
-
 import requests
 from flask import Flask, request
 
@@ -19,10 +14,6 @@ def full_ssrf():
     subdomain = "europe" if target == "EU" else "world"
     resp = requests.get("https://" + subdomain + ".example.com/data/")
 
-'''
-This is a multiline
-comment in Python
-'''
 
 @app.route("/partial_ssrf")
 def partial_ssrf():
@@ -36,9 +27,3 @@ def partial_ssrf():
     if user_id.isalnum():
         # GOOD: user_id is restricted to be alpha-numeric, and cannot alter path component of URL
         resp = requests.get("https://api.example.com/user_info/" + user_id)
-
-
-'''
-This is a multiline
-comment in Python
-'''
